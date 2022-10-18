9d0485055ed00664b66286c19c8b8b1f4f6e9e4a
srivijiayalakshmi AT <114483681+21viji@users.noreply.github.com>
2022-10-18 03:38:06 +0000

    prob 2 completed

---
+++ set01/problem02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{  
+  int a,b,add;
+  add=0;
+  printf("enter the first  numbers");
+  printf("enter the second number");
+  scanf("%d,%d",&a,&b);
+  add=a+b;
+  printf("summation of two numbers is %d",add);
+  return 0;
+}
